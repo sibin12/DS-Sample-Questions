cbf69bf3e6747348353c636d2795b0ba41685874
Sibin Silu <sibinsilu001@gmail.com>
2024-03-26 23:08:16 +0530

    bubble sort

---
+++ sorting/bubble_sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr){
+    let swapped;
+    do{
+        swapped =  false
+        for(let i=0;i<arr.length-1; i++){
+            if(arr[i]> arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = true
+            }
+        }
+        
+    }while(swapped)
+}
+
+ let  arr=[5,3,54,4,6,2]
+bubbleSort(arr)
+console.log(arr);
